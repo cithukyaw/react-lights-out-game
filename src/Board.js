119e4798db593ed4b05d35479b45b154a98a5049
Sithu <cithukyaw@gmail.com>
2020-04-17 22:48:14 +0630

    Winning the Game

--- src/Board.js
+++ src/Board.js
@@ -54,12 +54,16 @@ class Board extends Component {
 
     // win when every cell is turned off
     // determine if the game has been won
-    let hasWon = false;
+    let hasWon = board.every(row => row.every(cell => !cell));
 
     this.setState({ board, hasWon });
   }
 
   render() {
+    if (this.state.hasWon) {
+      return <h1>YOU WON!!!</h1>;
+    }
+
     let tblBoard = [];
     for (let y=0; y < this.props.nrows; y++) {
       let row = [];
